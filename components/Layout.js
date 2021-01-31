9d8d0f1977c2b6faf41000dbb0ca4d5fbfa7152f
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2021-01-31 10:36:14 +0100

    import: bootswatch stylesheet

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,11 @@
+import HEAD from 'next/head';
 const Layout = (props) => {
     return(
         <div>
+            <HEAD>
+                <title>Woocommerce React theme</title>
+                <link rel= "stylesheet" href= "https://bootswatch.com/4/lux/bootstrap.min.css"/>
+            </HEAD>
             {props.children}
         </div>
     );
