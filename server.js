eb0b761e45779296f1c94e04148cb8252a0e9e35
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2021-02-01 16:44:57 +0100

    fix: server.js

--- server.js
+++ server.js
@@ -1,48 +1,45 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const express = require('express')
-const next = require('next')
-const wooConfig = require('../woo-reactjs/wooConfig')
-var WooCommerceAPI = require('woocommerce-api');
-var WooCommerce = new WooCommerceAPI({
-    url: wooConfig.url,
-    consumerKey: wooConfig.consumerKey,
-    consumerSecret: wooConfig.consumerSecret,
-    wpAPI: true,
-    version: 'wc/v1'
-  });
+const next = require( 'next' );
+const express = require( 'express' );
+const wooConfig = require( './wooConfig' );
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const  WooCommerceAPI = require('woocommerce-api');
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
-    const server = express();
-    server.get('/getProducts',(req,res) => {
-        WooCommerce.get("products")
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error.response.data);
-        });
-      
-    });
+const WooCommerce = new WooCommerceAPI({
+	url: wooConfig.url,
+	consumerKey: wooConfig.consumerKey,
+	consumerSecret: wooConfig.consumerSecret,
+	wpAPI: true,
+	version: 'wc/v1'
+});
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
+const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+	.then( () => {
+		const server = express();
+
+		server.get( '/Products', ( req, response ) => {
+			WooCommerce.get('products', function(err, data, res) {
+				response.json( JSON.parse(res) );
+			});
+		} );
+
+		server.get( '*', ( req, res ) => {
+			return handle( req, res );
+		} );
+
+		server.listen( port, err => {
+			if ( err ) {
+				throw err;
+			}
+			console.log( `Ready on ${port}` );
+		} )
+
+	} )
+	.catch(ex => {
+	console.error(ex.stack);
+	process.exit(1);
+});
