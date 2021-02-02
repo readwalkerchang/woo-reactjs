a4fec89834bccfd5f6fae4c25261d1a55412b777
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2021-02-02 18:12:49 +0100

    feat: recommendation product section

--- components/Product.js
+++ components/Product.js
@@ -1,17 +1,29 @@
+import Media from 'react-bootstrap/Media'
+import "../style.css"
 const Product = ( props ) => {
-
+	
 	const { product } = props;
 	return (
-		<div className="card mb-3 mr-4" >
-			<h3 className="card-header text-center">{product.name}</h3>
-			<img
+		<div  className= "productContainer">
+			<Media>
+			<img  className= "productImg"
+				width={160}
+				height={160}
+				className=""
 			     src={ product.images[0].src }
 			     alt="Product image"/>
-			<div className="card-body text-center">
-				<h6 className="card-subtitle mb-3">${ product.price }</h6>
-				<a href="" className="btn btn-secondary">View</a>
-			</div>
-
+			  <Media.Body  className= "productDespription">
+			  <h3 className="">{product.name}</h3>
+			  <h6 className="">${ product.price }</h6>
+			  <a href="" className="btn btn-secondary">View</a>
+				<p>
+				Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+				ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+				tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+				Donec lacinia congue felis in faucibus.
+				</p>
+			</Media.Body>
+			</Media>
 		</div>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,19 @@ const Index = ( props ) => {
 
 	return (
 		<Layout>
+			<div className="contentSection">
+				<h1>This is an example title</h1>
+				<img src="https://images.unsplash.com/photo-1434648957308-5e6a859697e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80"/>
+				<p>This is an example page. It's different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:
+
+Hi there! I'm a bike messenger by day, aspiring actor by night, and this is my website. I live in Los Angeles, have a great dog named Jack, and I like piña coladas. (And gettin' caught in the rain.)
+
+...or something like this:
+
+The XYZ Doohickey Company was founded in 1971, and has been providing quality doohickeys to the public ever since. Located in Gotham City, XYZ employs over 2,000 people and does all kinds of awesome things for the Gotham community.
+
+As a new WordPress user, you should go to your dashboard to delete this page and create new pages for your content. Have fun!</p>
+			</div>
 			<div className="product-container">
 				{ products.length ? (
 					products.map( product => <Product key={ product.id } product={ product } /> )
