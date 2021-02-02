98a8c443c726ef3a3196d0eaf9d01c1516bacb2f
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2021-02-02 18:34:38 +0100

    feat: nav bar and search UI

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,29 @@
 import { Nav } from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
+import { FormControl } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+
 const NavBar = () => {
     return(
-        <Nav activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+        <Navbar bg="dark" variant="light" activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
             <Nav.Item>
-                <Nav.Link href="/home">Active</Nav.Link>
+                <Nav.Link href="/home">home</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
+                <Nav.Link eventKey="link-1">shop</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
+                <Nav.Link eventKey="link-2">competition</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>Disabled</Nav.Link>
+                <Nav.Link eventKey="link-3">news</Nav.Link>
             </Nav.Item>
-        </Nav>
+            <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+            </Form>
+        </Navbar>
     );
 }
 
